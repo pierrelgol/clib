ef07dc4c674f2c9fc453a6367d364c270ba809fa
PierreLgol <plgol.perso@gmail.com>
2024-05-06 12:19:01 +0200

    added definiton for arena

--- include/clib.h
+++ include/clib.h
@@ -10,71 +10,88 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __CLIB_H__
-#define __CLIB_H__
+#ifndef CLIB_H
+# define CLIB_H
 
-#include "clib_internals.h"
+# include "clib_internals.h"
 
 /******************************************************************************/
 /*                                   MISC                                     */
 /******************************************************************************/
 
-void expect(bool condition);
+void		expect(bool condition);
 
 /******************************************************************************/
 /*                                 ALLOCATOR                                  */
 /******************************************************************************/
 
-t_allocator *gpa_init(t_logger *logger);
+t_allocator	*gpa_init(t_logger *logger);
 
-void *gpa_alloc(void *const handle, const uint64_t size);
-void *gpa_dealloc(void *const handle, void *const ptr);
-void *gpa_resize(void *const handle, void *const ptr, const uint64_t nsize);
-void *gpa_dup(void *const handle, void *const ptr, const uint64_t size);
-void *gpa_dupz(void *const handle, void *const ptr);
-void *gpa_deinit(t_allocator *const allocator);
+void		*gpa_alloc(void *const handle, const uint64_t size);
+void		*gpa_dealloc(void *const handle, void *const ptr);
+void		*gpa_resize(void *const handle, void *const ptr,
+				const uint64_t nsize);
+void		*gpa_dup(void *const handle, void *const ptr, const uint64_t size);
+void		*gpa_dupz(void *const handle, void *const ptr);
+void		*gpa_deinit(t_allocator *const allocator);
 
-t_mem_node *mem_node_create(uint64_t size);
-t_mem_node *mem_node_destroy(t_mem_node *node);
-t_mem_node *mem_node_get_nchild(t_mem_node *node, uint64_t n);
-t_mem_node *mem_node_next_or_null(t_mem_node *node);
-bool        mem_node_contains_space(t_mem_node *node, uint64_t required);
-bool        mem_node_contains_ptr(t_mem_node *node, uintptr_t addr);
-void        mem_node_insert_child(t_mem_node *node, t_mem_node *child);
-t_mem_node *mem_node_remove_child(t_mem_node *node);
-uint64_t    mem_node_count_child(t_mem_node *node);
-t_mem_node *mem_node_remove_suitable(t_mem_node **head, uint64_t required);
-t_mem_node *mem_node_remove_matching(t_mem_node **head, uintptr_t addr);
-void        mem_node_destroy_all(t_mem_node *head);
-t_mem_node *node_clear(t_mem_node *node);
-t_mem_node *mem_node_allocate_or_reuse(t_mem_node **head, uint64_t size);
-uint64_t    mem_node_find_ptr_size(t_mem_node *head, uintptr_t addr);
+t_allocator	*arena_init(t_logger *logger, uint64_t size);
+void		*arena_alloc(void *const handle, const uint64_t size);
+void		*arena_dealloc(void *const handle, void *const ptr);
+void		*arena_resize(void *const handle, void *const ptr,
+				const uint64_t nsize);
+void		*arena_dup(void *const handle, void *const ptr,
+				const uint64_t size);
+void		*arena_dupz(void *const handle, void *const ptr);
+void		*arena_deinit(t_allocator *const allocator);
+
+t_mem_node	*mem_node_create(uint64_t size);
+t_mem_node	*mem_node_destroy(t_mem_node *node);
+t_mem_node	*mem_node_get_nchild(t_mem_node *node, uint64_t n);
+t_mem_node	*mem_node_next_or_null(t_mem_node *node);
+bool		mem_node_contains_space(t_mem_node *node, uint64_t required);
+bool		mem_node_contains_ptr(t_mem_node *node, uintptr_t addr);
+void		mem_node_insert_child(t_mem_node *node, t_mem_node *child);
+t_mem_node	*mem_node_remove_child(t_mem_node *node);
+uint64_t	mem_node_count_child(t_mem_node *node);
+t_mem_node	*mem_node_remove_suitable(t_mem_node **head, uint64_t required);
+t_mem_node	*mem_node_remove_matching(t_mem_node **head, uintptr_t addr);
+void		mem_node_destroy_all(t_mem_node *head);
+t_mem_node	*node_clear(t_mem_node *node);
+t_mem_node	*mem_node_allocate_or_reuse(t_mem_node **head, uint64_t size);
+uint64_t	mem_node_find_ptr_size(t_mem_node *head, uintptr_t addr);
 
 /******************************************************************************/
 /*                                 RAW_MEMORY                                 */
 /******************************************************************************/
 
-void *raw_alloc(const uint64_t size);
-void *raw_dealloc(void *const ptr);
-void *raw_resize(void *const ptr, const uint64_t osize, const uint64_t nsize);
-void *raw_dup(const void *ptr, const int64_t size);
-void *raw_dupz(const void *ptr);
-void *raw_copy(void *const dst, const void *src, const uint64_t dstsize);
-void *raw_ccopy(void *const dst, const int32_t byte, const void *src, const uint64_t dstsize);
-void   *raw_fill(void *const dst, const int32_t byte, const uint64_t dstsize);
-void   *raw_zero(void *const dst, const uint64_t dstsize);
-void   *raw_search(const void *s, const int32_t byte, const uint64_t dstsize);
-void   *raw_move(void *dst, const void *src, const uint64_t len);
-int32_t raw_compare(const void *a, const void *b, const uint64_t size);
+void		*raw_alloc(const uint64_t size);
+void		*raw_dealloc(void *const ptr);
+void		*raw_resize(void *const ptr, const uint64_t osize,
+				const uint64_t nsize);
+void		*raw_dup(const void *ptr, const int64_t size);
+void		*raw_dupz(const void *ptr);
+void		*raw_copy(void *const dst, const void *src, const uint64_t dstsize);
+void		*raw_ccopy(void *const dst, const int32_t byte, const void *src,
+				const uint64_t dstsize);
+void		*raw_fill(void *const dst, const int32_t byte,
+				const uint64_t dstsize);
+void		*raw_zero(void *const dst, const uint64_t dstsize);
+void		*raw_search(const void *s, const int32_t byte,
+				const uint64_t dstsize);
+void		*raw_move(void *dst, const void *src, const uint64_t len);
+int32_t		raw_compare(const void *a, const void *b, const uint64_t size);
 
 /******************************************************************************/
 /*                                   MEMORY                                   */
 /******************************************************************************/
 
-void *memalloc(t_allocator *const allocator, const uint64_t size);
-void *memdealloc(t_allocator *const allocator, void *const ptr);
-void *memresize(t_allocator *const allocator, void *const ptr, const uint64_t osize, const uint64_t nsize);
-void *memdup(t_allocator *const allocator, void *const ptr, const int64_t size);
-void *memdupz(t_allocator *const allocator, void *const ptr);
+void		*memalloc(t_allocator *const allocator, const uint64_t size);
+void		*memdealloc(t_allocator *const allocator, void *const ptr);
+void		*memresize(t_allocator *const allocator, void *const ptr,
+				const uint64_t osize, const uint64_t nsize);
+void		*memdup(t_allocator *const allocator, void *const ptr,
+				const int64_t size);
+void		*memdupz(t_allocator *const allocator, void *const ptr);
 
 #endif
--- include/clib_internals.h
+++ include/clib_internals.h
@@ -10,67 +10,71 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __CLIB_INTERNALS_H__
-#define __CLIB_INTERNALS_H__
+#ifndef CLIB_INTERNALS_H
+# define CLIB_INTERNALS_H
 
-#include <fcntl.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <fcntl.h>
+# include <stdbool.h>
+# include <stdint.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-typedef struct s_mem_node  t_mem_node;
-typedef struct s_allocator t_allocator;
-typedef struct s_logger    t_logger;
+typedef struct s_mem_node	t_mem_node;
+typedef struct s_allocator	t_allocator;
+typedef struct s_logger		t_logger;
 
-typedef void *(t_fn_alloc) (void *const handle, const uint64_t size);
-typedef void *(t_fn_dealloc) (void *const handle, void *const ptr);
-typedef void *(t_fn_resize) (void *const handle, void *const ptr, const uint64_t nsize);
-typedef void *(t_fn_dup) (void *const handle, void *const ptr, const uint64_t size);
-typedef void *(t_fn_dupz) (void *const handle, void *const ptr);
-typedef void *(t_fn_deinit) (t_allocator *const allocator);
-typedef void *(t_fn_clear) (void *const handle);
+typedef void				*(t_fn_alloc)(void *const handle,
+								const uint64_t size);
+typedef void				*(t_fn_dealloc)(void *const handle,
+								void *const ptr);
+typedef void				*(t_fn_resize)(void *const handle, void *const ptr,
+								const uint64_t nsize);
+typedef void				*(t_fn_dup)(void *const handle, void *const ptr,
+								const uint64_t size);
+typedef void				*(t_fn_dupz)(void *const handle, void *const ptr);
+typedef void				*(t_fn_deinit)(t_allocator *const allocator);
+typedef void				*(t_fn_clear)(void *const handle);
 
-struct s_mem_node
+struct						s_mem_node
 {
-	struct s_mem_node *next;
-	uint64_t           size;
-	uint64_t           used;
-	char              *block;
+	struct s_mem_node		*next;
+	uint64_t				size;
+	uint64_t				used;
+	char					*block;
 };
 
-struct s_logger
+struct						s_logger
 {
-	bool    is_logging;
-	bool    is_open;
-	int32_t fd;
+	bool					is_logging;
+	bool					is_open;
+	int32_t					fd;
 };
 
-struct s_allocator
+struct						s_allocator
 {
-	void         *handle;
-	t_fn_alloc   *alloc;
-	t_fn_dealloc *dealloc;
-	t_fn_resize  *resize;
-	t_fn_dup     *dup;
-	t_fn_dupz    *dupz;
-	t_fn_deinit  *deinit;
-	t_fn_clear   *clear;
-	t_logger     *log;
+	void					*handle;
+	t_fn_alloc				*alloc;
+	t_fn_dealloc			*dealloc;
+	t_fn_resize				*resize;
+	t_fn_dup				*dup;
+	t_fn_dupz				*dupz;
+	t_fn_deinit				*deinit;
+	t_fn_clear				*clear;
+	t_logger				*log;
 };
 
-struct s_gpa_allocator
+struct						s_gpa_allocator
 {
-	t_mem_node *freelist;
-	t_mem_node *usedlist;
+	t_mem_node				*freelist;
+	t_mem_node				*usedlist;
 };
 
-struct s_arena_allocator
+struct						s_arena_allocator
 {
-	uint64_t size;
-	uint64_t used;
-	char    *block;
+	uint64_t				size;
+	uint64_t				used;
+	char					*block;
 };
 
 #endif
